be29290954fcc0fb1b3ee2ca62166d0fc885060a
Sparky <145775791+MathisSparky@users.noreply.github.com>
2023-11-19 16:43:41 -0500

    Add files via upload

--- src/hello.asm
+++ src/hello.asm
@@ -1,15 +1,32 @@
 section .data
-    hello db 'Hello!', 0
+    msg db 'Welcome to my first assembly console program.', 0
+    msgtwo db 'I think I like how assembly is made.', 0
+    msgthree db 'It is pretty inspiring.', 0
 
 section .text
     global main
     extern puts
 
 main:
+    ; Msg
     sub rsp, 28
-
-    mov rcx, hello
+    mov rcx, msg
     call puts
-
     add rsp, 28
-    ret
+
+    ; Msg
+    sub rsp, 28
+    mov rcx, msgtwo
+    call puts
+    add rsp, 28
+
+    ; Msg
+    sub rsp, 28
+    mov rcx, msgthree
+    call puts
+    add rsp, 28
+
+    ; Exit
+    mov eax, 60
+    xor edi, edi
+    syscall
